9fa3784c57ce37f6b1dab0f1be82c91e17c4ec35
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2017-05-19 16:50:29 -0700

    init merge-meet-times

--- merge-meet-times.js
+++ merge-meet-times.js
@@ -1,7 +1,10 @@
 // write a function that will take in a series of meeting times as touples, and output an array of condensed times
 
-const mergeMeetings = () => {
+//ex:
+var meetingTimes = [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)];
 
+const mergeMeetings = (array) => {
+  console.log(array);
 }
 
-
+mergeMeetings(meetingTimes)
