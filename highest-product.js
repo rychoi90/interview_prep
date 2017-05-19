b8680e205c3d49170befd0693b43fca99d448cd1
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2017-05-19 16:25:36 -0700

    Pass all tests

--- highest-product.js
+++ highest-product.js
@@ -1,7 +1,11 @@
 // given an array of numbers, find the greatest product using any 3 of the integers.
 
 const highestProduct= (array) => {
-
+  var length = array.length;
+  var sorted = array.sort((prev,curr)  => {
+    return prev < curr;
+  });
+  return Math.max((sorted[0]*sorted[1]*sorted[2]),(sorted[0]*sorted[length-1]*sorted[length-2]));
 }
 
 // standard case
@@ -10,11 +14,18 @@ console.log(highestProduct([1, 2, 3, 4, 5]), 'should be 60');
 // should handle negative numbers
 console.log(highestProduct([-1, 2, 3, -4, 5]), 'should be 30');
 
+// should handle when largest numbers are not included
+console.log(highestProduct([1, 2, 3, 4, -5]), 'should be 24');
+
 // should handle when two negatives are used for product
-console.log(highestProduct([-1, -2, 3, -4, 5]), 'should be 40');
+console.log(highestProduct([-1, -2, 3, 4, -5]), 'should be 40');
+
+// should handle when extremes are ignored are used for product
+console.log(highestProduct([-1, 2, 3, 4, -5]), 'should be 24');
 
 // should handle when all products are negative
 console.log(highestProduct([-1, -2, -3, -4, -5]), 'should be -6');
 
 // should handle when all products are the same
 console.log(highestProduct([2, 2, 2, 2, 2]), 'should be 8');
+
