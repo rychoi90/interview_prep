d5721826eb528fc8d9cd12a8b50f06f7000c0bc4
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2017-05-21 00:14:28 -0700

    Implement using loop

--- merge-meet-times.js
+++ merge-meet-times.js
@@ -1,10 +1,46 @@
 // write a function that will take in a series of meeting times as touples, and output an array of condensed times
 
 //ex:
-var meetingTimes = [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)];
 
 const mergeMeetings = (array) => {
-  console.log(array);
+  var sorted = array.sort((prev, curr) => {
+    return prev[0] - curr[0];
+  });
+
+  // console.log(sorted);
+
+  var merged = [];
+  var previous = sorted[0];
+  var last;
+
+  sorted.slice(1).forEach((current) => {
+    if(previous[1] >= current[0]) {
+      previous = [previous[0],Math.max(previous[1],current[1])];
+    } else {
+      merged.push(previous);
+      previous = current;
+    }
+  });
+  merged.push(previous);
+
+  return merged;
 }
 
-mergeMeetings(meetingTimes)
+// two times touching
+console.log(mergeMeetings([[0, 1], [2, 3]]), 'should be: [[0,1], [2,3]]');
+
+// times overlapping
+console.log(mergeMeetings([[6, 9], [5, 7]]), 'should be: [[5,9]');
+
+// times not connected
+console.log(mergeMeetings([[14, 15], [11, 12]]), 'should be: [[11,12], [14,15]]');
+
+// one time nested within other time
+console.log(mergeMeetings([[17, 20], [18, 19]]), 'should be: [[17,20]');
+
+// three times touching
+console.log(mergeMeetings([[21, 22], [21, 23], [22, 24]]), 'should be: [[21,24]]');
+
+// all cases
+console.log(mergeMeetings([[0, 1], [2, 3], [6, 9], [5, 7], [14, 15], [11, 12], [17, 20], [18, 19], [21, 22], [21, 23], [22, 24]]), 'should be: [[0,1], [2,3], [5,9], [11,12], [14,15], [17,20], [21,24]]');
+
