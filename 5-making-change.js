5f9419a2364ae0e656e2984cd530c4fb0eaf4cfc
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2017-05-22 22:24:25 -0700

    Start problem, first test added

---
+++ 5-making-change.js
@@ -0,0 +1,9 @@
+// write a function that takes in an amount of money and the denominations of the available coins
+// and return the number of possible ways to make that mount of money.
+
+const makeChange = (amount, denominations) => {
+
+}
+
+
+console.log(makeChange(4, [1, 2, 3]), 'should be: 4');
